4a8d1cd5d680ef7f6abeeb0e9449f3f7e4dbc30e
wangzhigang <wangzhigang@htffund.com>
2015-05-28 18:03:07 +0800

    topic view update

--- dqs/controllers/topic_controller.go
+++ dqs/controllers/topic_controller.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	"dqs/dao"
+	"dqs/models"
 	"dqs/util"
 	log "github.com/cihub/seelog"
+	"time"
 )
 
 type TopicController struct {
@@ -15,6 +17,11 @@ func (this *TopicController) TopicView() {
 	this.Data["author"] = "wangzhigang"
 
 	this.CheckUser()
+
+	//当前时间
+	now := time.Now()
+	CurrentTime := now.Format(CommonTimeLayout)
+	this.Data["CurrentTime"] = CurrentTime
 
 	paginationDevices := util.Pagination{PageSize: 10, CurrentPage: 1}
 	err := dao.DeviceList(&paginationDevices)
@@ -41,4 +48,53 @@ func (this *TopicController) TopicView() {
 
 	this.TplNames = "topic.html"
 	this.Render()
+}
+
+//查询关联数据
+func (this *TopicController) TopicData() {
+	beginTime := this.GetString("beginTime")
+	endTime := this.GetString("endTime")
+	sensors := this.GetStrings("Sensors")
+
+	var eventSignal models.EventSignal = models.EventSignal{}
+	//查找报警数据
+	alarms, err := dao.GetTopicData(beginTime, endTime, sensors)
+	if err != nil {
+		log.Warnf("查找等值线的报警数据时出错:%s", err.Error())
+	}
+
+	//是否加入网格化虚拟站点
+	dataArray := NetGridCompute(alarms, eventSignal)
+
+	//传递的数据值
+	DataArrayStr := ""
+	DataArrayStrPGA := ""
+	DataArrayStrSI := ""
+	var lastlng, lastlat float32
+
+	for k, v := range dataArray {
+		if k < len(dataArray)-1 {
+			DataArrayStr += v.String() + ","
+			DataArrayStrPGA += v.StringPGA() + ","
+			DataArrayStrSI += v.StringSI() + ","
+		} else {
+			DataArrayStr += v.String()
+			DataArrayStrPGA += v.StringPGA()
+			DataArrayStrSI += v.StringSI()
+		}
+		//设置地图中心点位置
+		if k == 0 {
+			lastlng = v.Longitude
+			lastlat = v.Latitude
+		}
+	}
+	//添加系统参数
+	this.Data["dataArray"] = DataArrayStr
+	this.Data["dataArrayPGA"] = DataArrayStrPGA
+	this.Data["dataArraySI"] = DataArrayStrSI
+
+	this.Data["dataSize"] = len(dataArray)
+
+	this.Data["lastlng"] = lastlng
+	this.Data["lastlat"] = lastlat
 }
---
+++ dqs/dao/topic_dao.go
@@ -0,0 +1,38 @@
+package dao
+
+import (
+	"dqs/models"
+	"labix.org/v2/mgo/bson"
+	"time"
+)
+
+const (
+	TopicTimeLayout = "2006-01-02 15:04:05"
+)
+
+//分页查询
+func GetTopicData(beginTime string, endTime string, sensors []string) (*[]models.AlarmInfo, error) {
+	c := GetSession().DB(DatabaseName).C(DataCollection)
+
+	//构造查询参数
+	m := bson.M{}
+
+	timeparam := bson.M{}
+
+	btime, _ := time.ParseInLocation(TopicTimeLayout, beginTime, Local)
+	timeparam["$gte"] = btime
+
+	etime, _ := time.ParseInLocation(TopicTimeLayout, endTime, Local)
+	timeparam["$lt"] = etime
+
+	m["time"] = timeparam
+
+	//查询
+	alist := []models.AlarmInfo{}
+	err0 := c.Find(&m).All(&alist)
+	if err0 != nil {
+		return nil, err0
+	}
+
+	return &alist, nil
+}
